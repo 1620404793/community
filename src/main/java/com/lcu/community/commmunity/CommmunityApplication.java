db26f74fa1b754818edb8f9fa6c16b309e8874b8
贺朝成 <1620407593@qq.com>
2020-03-26 13:11:06 +0800

    init repo

---
+++ src/main/java/com/lcu/community/commmunity/CommmunityApplication.java
@@ -0,0 +1,13 @@
+package com.lcu.community.commmunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommmunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommmunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcu/community/commmunity/CommmunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcu.community.commmunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommmunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
